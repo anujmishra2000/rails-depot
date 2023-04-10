d434505a9d024fc2a1e4699a0af805f1366d29f1
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-04-10 20:21:47 +0530

    Task: E

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,14 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
-
-  # GET /carts or /carts.json
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+  # GET /carts
+  # GET /carts.json
   def index
     @carts = Cart.all
   end
 
-  # GET /carts/1 or /carts/1.json
+  # GET /carts/1
+  # GET /carts/1.json
   def show
   end
 
@@ -19,46 +21,52 @@ class CartsController < ApplicationController
   def edit
   end
 
-  # POST /carts or /carts.json
+  # POST /carts
+  # POST /carts.json
   def create
     @cart = Cart.new(cart_params)
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to cart_url(@cart), notice: "Cart was successfully created." }
+        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
         format.json { render :show, status: :created, location: @cart }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /carts/1 or /carts/1.json
+  # PATCH/PUT /carts/1
+  # PATCH/PUT /carts/1.json
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to cart_url(@cart), notice: "Cart was successfully updated." }
+        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
         format.json { render :show, status: :ok, location: @cart }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /carts/1 or /carts/1.json
+  # DELETE /carts/1
+  # DELETE /carts/1.json
   def destroy
-    @cart.destroy
-
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to store_index_url,
+        notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
 
+  # ...
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # ...
+
     def set_cart
       @cart = Cart.find(params[:id])
     end
@@ -66,5 +74,9 @@ class CartsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def cart_params
       params.fetch(:cart, {})
+    end
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
     end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,13 +24,10 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-notice: 'Line item was successfully created.' }
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -70,6 +67,6 @@ notice: 'Line item was successfully created.' }
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+      params.require(:line_item).permit(:product_id)
+      end
 end
