b64bac498321c8869efed5ba0fd6bfba16a20824
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-04-13 00:22:12 +0530

    Task: G(Check Out)

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,9 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :cart
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
+  belongs_to :cart, optional: true
   def total_price
     product.price * quantity
-    end
+  end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
