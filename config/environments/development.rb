76e06afbf838d46515895cb93f3cb654652f72b5
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-04-13 11:01:03 +0530

    Task: I(Sending emails)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: "domain.of.sender.net",
+authentication: "plain",
+user_name: "dave",
+password: "secret",
+enable_starttls_auto: true
+}
+
 end
