eb7fc92287c727381dec0ec68ddc335662a8504a
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-04-13 10:38:36 +0530

    Task: H(Payment details)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails 6",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
-#---
 class OrdersController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
@@ -47,7 +39,7 @@ class OrdersController < ApplicationController
         format.json { render :show, status: :created,
           location: @order }
       else
-        format.html { render :new ,status: 422}
+        format.html { render :new }
         format.json { render json: @order.errors,
           status: :unprocessable_entity }
       end
@@ -96,4 +88,18 @@ class OrdersController < ApplicationController
          redirect_to store_index_url, notice: 'Your cart is empty'
        end
      end
+
+      
+    def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_number)
+      else
+        {}
+      end
+    end
+
 end
