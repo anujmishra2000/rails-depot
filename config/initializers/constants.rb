5272441e17394ff4b2393c06380f5aef32e1db79
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-05-22 18:01:59 +0530

    Routing Extensions

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,3 +3,6 @@ DESCRIPTION_REGEX = /\A(\w+ ){4,9}\w+ *\z/
 URL_REGEX = /\w+\.(gif|jpg|png)\z/i
 ADMIN_EMAIL = "admin@depot.com"
 PAGE_PATH_REGEX = /\/(.*)(\?)?/
+FIREFOX_BROWSER_REGEX = /firefox/i.freeze
+DIGIT_REGEX = /[\d]+/.freeze
+
