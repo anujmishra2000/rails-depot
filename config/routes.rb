54d2a85bd987111573f60dbbec59b586b544aab3
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-05-23 13:48:36 +0530

    Routing Extensions after review:1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index', via: :all
   constraints(-> (request) { request.headers['User-Agent'] !~ FIREFOX_BROWSER_REGEX}) do
     controller :sessions do
       get  'login' => :new
@@ -9,9 +10,6 @@ Rails.application.routes.draw do
     get 'sessions/create'
     get 'sessions/destroy'
 
-    get '/my-orders', to: 'users#orders'
-    get '/my-items', to: 'users#line_items'
-
     resources :users
 
     resources :products, path: '/books' do
@@ -21,21 +19,21 @@ Rails.application.routes.draw do
     resources :support_requests, only: [ :index, :update ]
 
     resources :category do
-      resources :products, path: '/books', as: 'books', constraints: { category_id: DIGIT_REGEX }
-      resources :products, path: '/books', as: 'books', to: redirect('/')
+      resources :products, path: '/books', constraints: { category_id: DIGIT_REGEX }
+      resources :products, path: '/books', to: redirect('/')
     end
 
     namespace :admin do
-      get 'reports', to: 'reports#index'
-      get 'categories', to: 'categories#index'
+      get :reports, to: 'reports#index'
+      get :categories, to: 'categories#index'
     end
 
     scope '(:locale)' do
       resources :orders
       resources :line_items
       resources :carts
-      root 'store#index', as: 'store_index', via: :all
+      get '/my-orders', to: 'users#orders'
+      get '/my-items', to: 'users#line_items'
     end
   end
-  root "store#index"
 end
